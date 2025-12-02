d5d364315cc67dd43403f2d73200477457efd0d6
AGT-tech <abil.teshome@maine.edu>
2025-12-01 23:29:38 -0500

    Fetch fun fact about today's date from Numbers API
    Added functionality to fetch a fun fact about today's date from the Numbers API.

--- today.py
+++ today.py
@@ -5,4 +5,13 @@ def main():
   today = datetime.now().strftime("%Y-%m-%d)
   print(f"Today is: {today}")
 
+  try:
+    res = requests.get(f"http://numbersapi.com/{datetime.com.now().month]/{datetimme.now().day}/date")
+    if res.status_code ==200:
+      print("Fun fact about today:", res.text)
+    else:
+      print("Could not fetch a fun fact : (")
+  except Exception as e:
+    print("Error fetchiing fun fact:", e)
+
                                   
